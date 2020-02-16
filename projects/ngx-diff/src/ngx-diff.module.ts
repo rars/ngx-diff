e30baea0ad40a2febc7e2923dd8d94ad6a090453
Richard Russell <richardr39@gmail.com>
2020-02-16 18:57:25 +0000

    refactor: double to single quotes; update dependencies to latest angular; add inline component to public api export

--- projects/ngx-diff/src/ngx-diff.module.ts
+++ projects/ngx-diff/src/ngx-diff.module.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { DiffMatchPatchService } from "./diff-match-patch.service";
-import { InlineDiffComponent } from "./inline-diff/inline-diff.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { DiffMatchPatchService } from './diff-match-patch.service';
+import { InlineDiffComponent } from './inline-diff/inline-diff.component';
 
 @NgModule({
   declarations: [InlineDiffComponent],
--- projects/ngx-diff/src/public-api.ts
+++ projects/ngx-diff/src/public-api.ts
@@ -4,3 +4,4 @@
 
 export { DiffMatchPatchService } from './diff-match-patch.service';
 export { NgxDiffModule } from './ngx-diff.module';
+export { InlineDiffComponent } from './inline-diff/inline-diff.component';
