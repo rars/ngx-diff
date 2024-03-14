f870bd8076a47ff9639ff3b61aeec159d1d17c11
Richard Russell <2265225+rars@users.noreply.github.com>
2024-03-14 11:11:59 +0000

    feat(ngx-diff): add named light and dark themes

--- projects/ngx-diff/src/lib/pipes/line-number/line-number.pipe.ts
+++ projects/ngx-diff/src/lib/pipes/line-number/line-number.pipe.ts
@@ -1,8 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'lineNumber',
-    standalone: true,
+  name: 'lineNumber',
+  standalone: true,
 })
 export class LineNumberPipe implements PipeTransform {
   public transform(value: number | null): string {
--- projects/ngx-diff/src/lib/services/diff-match-patch/diff-match-patch.service.ts
+++ projects/ngx-diff/src/lib/services/diff-match-patch/diff-match-patch.service.ts
@@ -6,11 +6,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class DiffMatchPatchService {
-  private readonly dmp: DiffMatchPatch;
-
-  public constructor() {
-    this.dmp = new DiffMatchPatch();
-  }
+  private readonly dmp = new DiffMatchPatch();
 
   public computeLineDiff(text1: string, text2: string): Diff[] {
     return this.dmp.diff_lineMode(text1, text2);
