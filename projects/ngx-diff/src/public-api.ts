842d520f333763080a2e5a00d66879ec54f7a7f1
Richard Russell <2265225+rars@users.noreply.github.com>
2024-03-15 10:01:30 +0000

    fix(ngx-diff): add UnifiedDiffComponent to exported types

--- projects/ngx-diff/src/public-api.ts
+++ projects/ngx-diff/src/public-api.ts
@@ -5,5 +5,6 @@
 export * from './lib/services/diff-match-patch/diff-match-patch.service';
 export * from './lib/components/inline-diff/inline-diff.component';
 export * from './lib/components/side-by-side-diff/side-by-side-diff.component';
+export * from './lib/components/unified-diff/unified-diff.component';
 export * from './lib/common/line-select-event';
 export * from './lib/common/line-diff-type';
