778802dcc94da652212c18cb2c00992cfda8c2c4
Richard Russell <2265225+rars@users.noreply.github.com>
2024-03-15 09:50:26 +0000

    refactor: update demo to use 8.0.0 ngx-diff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
-import { InlineDiffComponent } from 'ngx-diff';
+import { UnifiedDiffComponent, SideBySideDiffComponent } from 'ngx-diff';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, InlineDiffComponent, MatToolbarModule],
+  imports: [
+    RouterOutlet,
+    NgClass,
+    FormsModule,
+    UnifiedDiffComponent,
+    SideBySideDiffComponent,
+    MatToolbarModule,
+    MatSlideToggleModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  public isDarkMode = false;
   public oldText = 'apples\noranges\nkiwis\nstrawberries\n';
   public newText = 'apples\npears\nkiwis\ngrapefruit\nstrawberries\n';
 }
