a8d9b28f5c79c75692de6a991c7e3329c59a4663
Richard Russell <2265225+rars@users.noreply.github.com>
2026-06-15 13:24:48 +0100

    feat(ngx-diff): update to Angular 22

--- projects/ngx-diff/src/lib/components/progress-bar/progress-bar.component.ts
+++ projects/ngx-diff/src/lib/components/progress-bar/progress-bar.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'ngx-progress-bar',
   imports: [],
   templateUrl: './progress-bar.component.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './progress-bar.component.scss',
 })
 export class ProgressBarComponent {}
